d50bb449c82076852b8f046b3b61503a22a528ad
Harshal Valanda <valanda.harshal15@gmail.com>
2023-04-20 21:34:18 +0530

    Issue fix

--- src/screen/Authentication/SignUp.js
+++ src/screen/Authentication/SignUp.js
@@ -18,9 +18,9 @@ const SignUp = (props) => {
     const [clickable, setClickable] = useState(false);
     const [isSecurePassword, setSecurePassword] = useState(true);
     const [isVisiblePassword, setIsVisiblePassword] = useState(true);
-    const [email, setEmail] = useState('dharaValanda@gmail.com');
-    const [password, setPassword] = useState('dhara');
-    const [confirmPassword, setConfirmPassword] = useState('dhara');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     function onSignUp() {
             if (!email) {
