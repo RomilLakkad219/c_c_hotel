de5d9187851866f6ea85cfbbae1f853a86fbf3f0
Harshal Valanda <valanda.harshal15@gmail.com>
2023-04-24 12:42:16 +0530

    Add some file changes

--- src/screen/Authentication/Login.js
+++ src/screen/Authentication/Login.js
@@ -17,7 +17,7 @@ const Login = (props) => {
 
     const [isSecurePassword, setSecurePassword] = useState(true);
     const [email, setEmail] = useState('dharaValanda@gmail.com');
-    const [password, setPassword] = useState('dhV@19')
+    const [password, setPassword] = useState('dV@19')
 
     function onLogin() {
             if (!email) {
